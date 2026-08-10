7c640feaa3a25f1505f1f0640d0362b01fa5c6bd
Indra W <indra.roadota@gmail.com>
2026-08-10 23:16:16 +0700

    chore: setup vercel analytics and node 20 build env

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,13 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { LanguageProvider } from './i18n/LanguageContext';
+import { Analytics } from '@vercel/analytics/react';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <LanguageProvider>
       <App />
+      <Analytics />
     </LanguageProvider>
   </StrictMode>
 );
